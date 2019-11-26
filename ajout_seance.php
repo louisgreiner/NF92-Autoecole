6fea90b6a8c687a4f70623ccbf6879413ee7ef3c
Louis-UTC <greiner.louis@gmail.com>
2019-11-26 14:43:20 +0100

    ajouter seance

--- ajout_seance.php
+++ ajout_seance.php
@@ -12,26 +12,49 @@
     <div class="container">
         <div class="row">
             <div class="col-20">
+                <label>Séances déjà existantes</label>
+            </div>
+            <div class="col-50">
+
+                <?php 
+                    $dbhost='localhost';
+                    $dbuser='root';
+                    $dbpass='';
+                    $dbname='autoecole';
+                    $connect=mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die ('Error connecting to mysql');
+
+                    echo "<TABLE border=3px>";
+                    $result=mysqli_query($connect, "SELECT * FROM themes INNER JOIN seances ON seances.idtheme=themes.idtheme");
+                    while ($row=mysqli_fetch_array($result, MYSQLI_NUM)) {
+                        echo "<TR><TD>$row[1]</TD><TD>$row[3]</TD><TD>$row[5]</TD><TD>$row[6]</TD></TR>";
+                    }
+                    echo "</TABLE>";
+                //   echo "<br>Horaire du début deazaz la séance <imput type='date' name='debutseance' required>";
+                //    echo "Horaire de la fin de laazeaze séance <imput type='date' name='finseance'>";
+
+                ?>
+                    
+            </div>
+            </div>
+        <div class="row">
+            <div class="col-20">
                 <label>Choisir un thème parmis ceux existants</label>
             </div>
             <div class="col-50">
 
             <?php 
-                $dbhost='localhost';
-                $dbuser='root';
-                $dbpass='';
-                $dbname='autoecole';
-                $connect=mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die ('Error connecting to mysql');
+
                 $result=mysqli_query($connect, "SELECT * FROM themes WHERE supprime='1'");
                 echo "<select name='choixtheme' size='4' required>";
                 while ($row=mysqli_fetch_array($result, MYSQLI_NUM)) {
-                echo "<option value='$row[0]'>$row[1]</option>";
+                    echo "<option value='$row[0]'>$row[1]</option>";
                 }
                 echo "</select>";
              //   echo "<br>Horaire du début deazaz la séance <imput type='date' name='debutseance' required>";
             //    echo "Horaire de la fin de laazeaze séance <imput type='date' name='finseance'>";
+
                 mysqli_close($connect);
-                ?>
+            ?>
 
             </div>
         </div>
@@ -40,7 +63,19 @@
                 <label>Horaire du début de la séance</label>
             </div>
             <div class="col-50">
-               <input type="date" name="debutseance" required>
+               <input type="date" name="jourseance" min="2019-11-25"  required>
+               <input type="time" name="debutseance" min="8:00" max="19:00" required>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-20">
+                <label>Durée de la séance</label>
+            </div>
+            <div class="col-50">
+                <input type="radio" name="duree" value="45" required>0h45 -
+                <input type="radio" name="duree" value="60" required>1h00 -
+                <input type="radio" name="duree" value="75" required>1h15 -
+                <input type="radio" name="duree" value="90" required>1h30
             </div>
         </div>
         <div class="row">
@@ -48,13 +83,14 @@
                 <label>Effectif maximum</label>
             </div>
             <div class="col-50">
-               <input type="text" name="effmax" placeholder="20" pattern="[0-9][0-9]+" required>
+               <input type="text" name="effmax" placeholder="20" pattern="[0-9]+" required>
             </div>
         </div>
+
         <div class="row">
-      <input type="submit" value="Terminé"><br>
+            <input type="submit" value="Terminé"><br>
+        </div>
     </div>
-  </div>
 </form>
 <A HREF=accueil.html class="button" TARGET=accueil><button>Retour à l'accueil</button></A>
 </body>
--- ajouter_seance.php
+++ ajouter_seance.php
@@ -5,28 +5,48 @@
     $dbname='autoecole';
     $connect=mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die ('Error connecting to mysql');
 
-    $choixtheme=$_POST['choixtheme'];
+    $jourseance=$_POST['jourseance'];
     $debutseance=$_POST['debutseance'];
+    $duree=$_POST['duree'];
     $effmax=$_POST['effmax'];
+    $choixtheme=$_POST['choixtheme'];
 
-    //$row=mysqli_fetch_row($result);
-   // while  ($row=mysqli_fetch_array($result, MYSQLI_NUM)) {
-  //      mysqli_query($connect, "UPDATE themes set supprime='' WHERE idtheme='$row[0]'");
-  //      echo "result".$row[1];
-  //  }
+    switch($duree){
+        case 45:
+            $duree="00:45";
+            break;
+        case 60:
+            $duree="01:00";
+            break;
+        case 75:
+            $duree="01:15";
+            break;
+        case 90:
+            $duree="01:30";
+            break;
+    }
 
-  if (empty($_POST['debutseance']) or empty($_POST['effmax']) or empty($_POST['choixtheme'])) {
+    $finseance=gmdate('H:i', strtotime($debutseance) + strtotime($duree));
+
+  //  $finseance=date("H:i", strtotime($debutseance) + strtotime($duree));
+    echo ".......________________.." . $finseance;
+
+if (empty($_POST['jourseance']) or empty($_POST['debutseance']) or empty($_POST['duree']) or empty($_POST['effmax']) or empty($_POST['choixtheme'])) {
     echo "<br> Me prends pas pour un con, un des champs n'est pas rempli";
     echo '<META HTTP-EQUIV="Refresh" CONTENT="3;URL=./ajout_eleve.html">';
+}
+else {
+    $query="INSERT INTO seances VALUES (NULL, "."'$jourseance'".", "."'$debutseance'".", "."'$finseance'".", "."'$effmax'".", "."'$choixtheme')";
+    $result=mysqli_query($connect, $query);
+    if (!$result) {
+        echo "<br>Y'a une couille:<br>".mysqli_error($connect);
     }
     else {
-        echo "<br> Merci pour votre participation, votre demande d'incription va être traitée dans les plus brefs délais.<br>";
-        $result=mysqli_query($connect, "INSERT INTO seances (NULL, "."'$debutseance".","."'$effmax'".","."'$choixtheme')");
-        echo "INSERT INTO seances (NULL, "."'$debutseance'".", "."'$effmax'".", "."'$choixtheme')";
-        if (!$result) {
-            echo "<br>Y'a une couille:<br>".mysqli_error($connect);}
-        }
+        echo "<br> Merci pour votre participation, votre demande d'inscription va être traitée dans les plus brefs délais.<br>";
+        echo "Récapitulatif de vos informations : " . "<br>Jour de la séance : " . "$jourseance" . "<br>Horaire du début de séance : " . "$debutseance" . "<br>Horaire de la fin de séance : " . "$finseance" . "<br>Effectif max : " . "$effmax"; // . "<br>Ville : " . "$ville" . "<br>Adresse postale : " . "$adresse_postale" . "<br>Téléphone : " . "$telephone" . "<br>Sexe : " . "$sexe" . "<br>Adresse mail : " . "$adresse_mail"; 
 
+    }
+}
     mysqli_close($connect);
 ?>
 
